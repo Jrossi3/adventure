5498b60fcd990e57a8eb8947d8131dd4ea5b78e6
Jrossi3 <55547488+Jrossi3@users.noreply.github.com>
2023-04-02 19:24:38 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,87 +1,169 @@
 import json
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 adventure.py [map filename]")
+
+map_filename = sys.argv[1]
 
 # Load the JSON data from the file
-with open("adventure.json") as f:
+with open(map_filename) as f:
     rooms = json.load(f)
 
 # Start the player in the first room
 current_room = rooms[0]
 inventory = []
 count = 1
+test = 0
+exitStr = "Exits:"
+itemList = ""
+directionList = ["north", "east", "south", "west"]
+quitHelp = 1
 
 # Main game loop
-while True:
-    if count:
-        # Print the current room's name and description
-        print("> " + current_room["name"])
-        print(current_room["desc"])
+while quitHelp:
+    try:
+        while True:
+            if count:
+                # Print the current room's name and description
+                print("> " + current_room["name"] + "\n")
+                print(current_room["desc"] + "\n")
 
-        # Check for any items in the room and print them
-        if "items" in current_room:
-            print("Items:")
-            for item in current_room["items"]:
-                print("" + item)
-        if inventory:
-            print("Inventory:")
-            for item in inventory:
-                print("" + item)
-        # Print the available exits for the current room
-        print("Exits:")
-        for exit_direction, room_index in current_room["exits"].items():
-            print(f"{exit_direction}: {rooms[room_index]['name']}")
-    
-    # Ask the player which direction they want to go in
-    direction = input("What would you like to do? ")
-    if direction.lower() == "quit":
-        print("Goodbye!")
-        break
-    elif direction.lower() == "look":
-        count = 1
+                # Check for any items in the room and print them
+                if "items" in current_room:
+                    if len(current_room["items"]) != 0:
+                        for item in current_room["items"]:
+                            itemList = itemList + " " + item
+                        print("Items:" + itemList + "\n")
+                        itemList = ""
+                # Print the available exits for the current room
+                if test:
+                    print("Exits:")
+                    for exit_direction, room_index in current_room["exits"].items():
+                        print(f"{exit_direction}: {rooms[room_index]['name']}\n")
+                else:
+                    exitStr = "Exits:"
+                    for exit_direction, room_index in current_room["exits"].items():
+                        exitStr = exitStr + " " + exit_direction
+                    print(exitStr + "\n")
+                    
+            # Ask the player which direction they want to go in
+            direction = input("What would you like to do? ")
+            direction = direction.lower()
+            if direction.lstrip() == "quit":
+                print("Goodbye!")
+                quitHelp = 0
+                break
+            elif direction.lstrip().startswith("drop"):
+                if direction.lstrip().replace("drop ", "") in inventory:
+                    inventory.remove(direction.replace("drop ", ""))
+                    count = 0
+                    if "items" in current_room:
+                        current_room["items"].append(item_name)
+                        print("You drop the " + direction.replace("drop ", "") + ".")
+                        continue
+                    else:
+                        current_room["items"] = []
+                        current_room["items"].append(item_name)
+                        print("You drop the " + direction.replace("drop ", "") + ".")
+                        continue
+                elif len(direction.lstrip().replace("drop", "")) < 2:
+                    print("Please designate an item to be removed")
+                    count = 0
+                else:
+                    print("Item cannot be removed")
+                    count = 0
+            elif direction.lstrip() == "show exits" and test:
+                count = 0
+                print("Exits are already showing")
+                continue
+            elif direction.lstrip() == "show exits":
+                count = 1
+                test = 1
+                continue
+            elif direction.lstrip() == "remove exits" and test != 1:
+                count = 0
+                print("Exits are not showing")
+                continue
+            elif direction.lstrip() == "remove exits":
+                count = 1
+                test = 0
+                continue
+            elif direction.lstrip() == "look":
+                count = 1
+                continue
+            elif direction.lstrip().startswith("get"):
+                if len(direction.lstrip().replace("get", "")) < 2:
+                    print("Sorry, you need to 'get' something.")
+                    count = 0
+                    continue
+                # Get the item name
+                item_name = direction.lower().replace("get ", "")
+                # Check if the item is in the room
+                if "items" in current_room and item_name in current_room["items"]:
+                    # Add the item to the inventory and remove it from the room
+                    inventory.append(item_name)
+                    itemList = itemList.replace(" " + item_name, "")
+                    current_room["items"].remove(item_name)
+                    print(f"You pick up the {item_name}.")
+                    count = 0
+                    continue
+                else:
+                    count = 0
+                    print("There's no " + direction.replace("get ", "") + " anywhere.")
+                    continue
+            # Check if the chosen action is to show inventory
+            elif direction.lstrip() == "inventory":
+                if inventory:
+                    print("Inventory:")
+                    for item in inventory:
+                        print("  " + item)
+                    count = 0
+                else:
+                    count = 0
+                    print("You're not carrying anything.")
+            elif direction.lstrip().startswith("go"):
+                direction = direction.replace("go", "").strip()
+                if len(direction) == 0:
+                    print("Sorry, you need to 'go' somewhere.")
+                    count = 0
+                    continue
+            # Check if the chosen direction is valid
+                elif direction in current_room["exits"]:
+                    # Move the player to the new room
+                    room_index = current_room["exits"][direction]
+                    current_room = rooms[room_index]
+                    count = 1
+                    print("You go " + direction + ".\n")
+                    continue
+                else:
+                    count = 0
+                    print("There's no way to go " + direction + ".")
+                    continue   
+            elif direction.lstrip() in current_room["exits"]:
+                # Move the player to the new room
+                room_index = current_room["exits"][direction]
+                current_room = rooms[room_index]
+                count = 1
+                print("You go " + direction + ".\n")
+            elif direction.lstrip() in directionList:
+                print("There's no way to go " + direction + ".")
+                count = 0
+            elif direction.strip() == "help":
+                print("You can run the following commands:")
+                print("  go + [direction]: This allows the user to move in the designated direction.")
+                print("  get + [item]: This allows the user to pick up an item from a room if it is available and add it to their inventory.")
+                print("  inventory: This shows the user's current inventory.")
+                print("  look: This shows the user what room they are in with the exit drections.")
+                print("  show exits: This shows the exit destinations.")
+                print("  remove exits: This removes the exit destinations.")
+                print("  drop: This drops an item from your inventory and adds it to the room you are in.")
+                print("  quit: This quits the user out of the program.")
+                print("  help: This shows the commands you can run on this.")
+                count = 0
+            else:
+                print("Please enter a valid direction or command like 'get'")
+                count = 0
+    except EOFError as e:
+        print("\nUse 'quit' to exit.")
+        count = 0
         continue
-    elif direction.lower().startswith("get "):
-        # Get the item name
-        item_name = direction.lower().replace("get ", "")
-        # Check if the item is in the room
-        if "items" in current_room and item_name in current_room["items"]:
-            # Add the item to the inventory and remove it from the room
-            inventory.append(item_name)
-            current_room["items"].remove(item_name)
-            print(f"You pick up the {item_name}.")
-            count = 0
-            continue
-        else:
-            count = 0
-            print("There's no " + direction.replace("get ", "") + " anywhere.")
-
-    # Check if the chosen action is to show inventory
-    elif direction.lower() == "inventory":
-        if inventory:
-            print("Inventory:")
-            for item in inventory:
-                print("" + item)
-            count = 0
-        else:
-            count = 0
-            print("Your inventory is empty.")
-    elif direction.lower() == "go":
-        count = 0
-        print("Sorry, you need to 'go' somewhere.")
-    # Check if the chosen direction is valid
-    elif "go" in direction.lower():
-        direction = direction.replace("go ", "")
-        if direction in current_room["exits"]:
-            # Move the player to the new room
-            room_index = current_room["exits"][direction]
-            current_room = rooms[room_index]
-            count = 1
-        else:
-            count = 0
-            print("You cant go that way")
-    elif direction in current_room["exits"]:
-        # Move the player to the new room
-        room_index = current_room["exits"][direction]
-        current_room = rooms[room_index]
-        count = 1
-    else:
-        print("You can't go that way.")
-        count = 0
